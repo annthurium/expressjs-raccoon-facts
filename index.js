824171f4dfa4da6679119985675ea6df3ed7a7c3
Tilde <tilde@thuryism.net>
2024-10-03 15:34:41 -0700

    Just the facts man.

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
 const express = require("express");
-const path = require("path");
-const serveStatic = require("serve-static");
-
-// add these new dependencies
-const ld = require("@launchdarkly/node-server-sdk");
-require("dotenv").config();
 
 const app = express();
 app.set("view engine", "pug");
-
-const sdkKey = process.env.LAUNCHDARKLY_SDK_KEY;
-const ldClient = ld.init(sdkKey);
 
 const getRaccoonFact = async function (apiVersion) {
   const url = `https://versioned-api.onrender.com/v${apiVersion}/fact/`;
@@ -20,19 +11,9 @@ const getRaccoonFact = async function (apiVersion) {
 };
 
 app.get("/", async function (req, res) {
-  const context = {
-    kind: "user",
-    key: "user-key-123abcde",
-    email: "raccacoonie@everything.pizza",
-  };
-  const apiVersion = await ldClient.variation(
-    "raccoon-api_version",
-    context,
-    0
-  );
+  const apiVersion = 0;
 
   const raccoonFact = await getRaccoonFact(apiVersion);
-
   res.render("index.pug", { raccoonFact, apiVersion });
 });
 
@@ -40,14 +21,4 @@ const port = 3000;
 const server = app.listen(port, function (err) {
   if (err) console.log("Error in server setup");
   console.log(`Server listening on http://localhost:${port}`);
-});
-
-// Add this new function to gracefully close the connection to the LaunchDarkly server.
-process.on("SIGTERM", () => {
-  debug("SIGTERM signal received: closing HTTP server");
-  ld.close();
-  server.close(() => {
-    debug("HTTP server closed");
-    ldClient.close();
-  });
 });
